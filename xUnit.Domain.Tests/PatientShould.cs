455a38ed059440260d9dddea467fe94492e2d9ed
KyrieIrving2 <1140584614@qq.com>
2019-10-13 11:21:33 +0800

    project init

---
+++ xUnit.Domain.Tests/PatientShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnit.Domain.Tests
+{
+    public class PatientShould
+    {
+        [Fact]
+        public void PatientIsNew()
+        {
+            var patient = new Patient();
+
+            Assert.True(patient.IsNew);
+        }
+    }
+}
---
+++ xUnit.Domain/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace xUnit.Domain
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            IsNew = true;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public int HeartBeatRate { get; set; }
+        public bool IsNew { get; set; }
+
+        public void IncreaseHeartBeatRate()
+        {
+            HeartBeatRate = CalculateHeartBeatRate() + 2;
+        }
+
+        private int CalculateHeartBeatRate()
+        {
+            var random = new Random();
+            return random.Next(1, 100);
+        }
+    }
+}
